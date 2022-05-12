18e1fd7ce2ff3a352b4bcf002ebd58d916b0bb7d
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2022-05-12 16:34:29 +0200

    added first assets

--- resources/views/components/headerMob.blade.php
+++ resources/views/components/headerMob.blade.php
@@ -1,5 +1,5 @@
 <div class="header--mob">
     <div class="header--mob__imgcontainer">
-        <img class="header--mob__img" src="" alt="">
+        <img class="header--mob__img" src="{{asset('img/logo2.png')}}" alt="">
     </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,6 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato&family=Montserrat:wght@700&display=swap" rel="stylesheet">
     <link rel="icon" type="image/png" sizes="16x16" href="favicon-tailwind.png">
-    <script src="https://cdn.tailwindcss.com"></script>
     
     
   </head>

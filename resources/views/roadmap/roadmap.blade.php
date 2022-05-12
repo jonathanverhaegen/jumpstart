fe5465d3afaec5152375db9219955df6a4a087d4
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2022-05-12 14:44:51 +0200

    btns for stages

--- resources/views/roadmap/roadmap.blade.php
+++ resources/views/roadmap/roadmap.blade.php
@@ -36,7 +36,47 @@
     <x-search />
 
     <div class="roadmap__container">
-        <div>roadmap</div>
+        <div class="roadmap">
+            <div class="roadmap__stage roadmap__stage--1">
+                <div class="roadmap__stage__title">Bank</div>
+                <div class="roadmap__stage__number roadmap__stage__number--right">1</div>
+            </div>
+
+            <div class="roadmap__stage roadmap__stage--2">
+                <div class="roadmap__stage__number roadmap__stage__number--left">2</div>
+                <div class="roadmap__stage__title">Bank</div>
+            </div>
+
+            <div class="roadmap__stage roadmap__stage--3">
+                <div class="roadmap__stage__title">Bank</div>
+                <div class="roadmap__stage__number roadmap__stage__number--right">3</div>
+            </div>
+
+            <div class="roadmap__stage roadmap__stage--4">
+                <div class="roadmap__stage__number roadmap__stage__number--left">4</div>
+                <div class="roadmap__stage__title">Bank</div>
+            </div>
+
+            <div class="roadmap__stage roadmap__stage--5">
+                <div class="roadmap__stage__title">Bank</div>
+                <div class="roadmap__stage__number roadmap__stage__number--right">5</div>
+            </div>
+
+            <div class="roadmap__stage roadmap__stage--6">
+                <div class="roadmap__stage__number roadmap__stage__number--left">6</div>
+                <div class="roadmap__stage__title">Bank</div>
+            </div>
+
+            <div class="roadmap__stage roadmap__stage--7">
+                <div class="roadmap__stage__title">Bank</div>
+                <div class="roadmap__stage__number roadmap__stage__number--right">7</div>
+            </div>
+
+            <div class="roadmap__stage roadmap__stage--8">
+            <div class="roadmap__stage__number roadmap__stage__number--left">8</div>
+                <div class="roadmap__stage__title">Bank</div>
+            </div>
+        </div>
     </div>
 
 

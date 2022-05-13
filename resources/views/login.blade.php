b91519ca9384f701fd49d5881ae202242db8f145
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2022-05-13 18:24:29 +0200

    login bugfix

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,25 @@
 
 @section('content')
 
+@if($errors->any())
+    @component('components/notification')
+    @slot('type') error @endslot
+        <ul>
+            @foreach($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endcomponent
+    @endif
 
+    @if($flash = session('error'))
+    @component('components/notification')
+    @slot('type') error @endslot
+        <ul>
+            <li>{{ $flash }}</li>
+        </ul>
+    @endcomponent
+    @endif
 
 
     <form class="form--login" action="/user/login" method="post">
@@ -19,7 +37,7 @@
         <div class="form--login__fields">
 
             <input class="form--login__input" type="text" name="email" placeholder="Email">
-            <input class="form--login__input password" type="password" name="wachtwoord" placeholder="Wachtwoord">
+            <input class="form--login__input password" type="password" name="password" placeholder="Wachtwoord">
             <img class="togglePassword" src="{{asset('img/verborgen.png')}}" alt="toggle">
             <p class="form--login__forgot">Wachtwoord vergeten?</p>
 

967b711927b88339aaeb0810fcc574cc697a05d2
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2022-05-13 14:24:23 +0200

    better header

--- resources/views/components/headerMob.blade.php
+++ resources/views/components/headerMob.blade.php
@@ -1,5 +1,5 @@
 <div class="header--mob">
-    <div class="header--mob__imgcontainer">
-        <img class="header--mob__img" src="{{asset('img/logo2.png')}}" alt="">
+    <div class="header--mob__img">
+
     </div>
 </div>

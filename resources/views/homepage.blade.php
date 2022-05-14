7c58b470de5bbbae588f7e90eac8380fabef359e
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2022-05-14 15:40:48 +0200

    desktop design to every page

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,31 +4,54 @@
 
 @section('content')
 
-@if($errors->any())
-    @component('components/notification')
-        <ul>
-            @foreach($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endcomponent
-@endif
 
-@if($flash = session('error'))
-@component('components/notification')
-        <ul>
-            <li>{{ $flash }}</li>
-        </ul>
-    @endcomponent
-@endif
 
-@if($flash = session('success'))
-@component('components/notification')
-        <ul>
-            <li>{{ $flash }}</li>
-        </ul>
-    @endcomponent
-@endif
+    <div class="container">
+
+    <div class="header__container">
+        <x-header />
+    </div>
+
+    <div class="body__container">
+        <x-search />
+
+        @if($errors->any())
+        @component('components/notification')
+        @slot('type') error @endslot
+            <ul>
+                @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endcomponent
+        @endif
+
+        @if($flash = session('error'))
+        @component('components/notification')
+        @slot('type') error @endslot
+            <ul>
+                <li>{{ $flash }}</li>
+            </ul>
+        @endcomponent
+        @endif
+
+        @if($flash = session('success'))
+        @component('components/notification')
+        @slot('type') success @endslot
+            <ul>
+                <li>{{ $flash }}</li>
+            </ul>
+        @endcomponent
+        @endif
+
+        @if($flash = session('message'))
+        @component('components/notification')
+        @slot('type') message @endslot
+            <ul>
+                <li>{{ $flash }}</li>
+            </ul>
+        @endcomponent
+        @endif
 
 
       <div class="dashboard__container">

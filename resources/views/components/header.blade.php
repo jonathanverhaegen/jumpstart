62de17d04511a0a003819d2421a5e66e8abb4e9b
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2022-05-14 15:35:53 +0200

    added desktop menu

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,44 @@
+<div class="header--mob">
+    <div class="header--mob__img">
+
+    </div>
+</div>
+
+<div class="header">
+    <div class="header__img">
+        <img src="{{asset('/img/logo1.png')}}" alt="">
+    </div>
+
+    <div class="header__links">
+        <div class="header__links__item">
+            <img class="header__links__item__img" src="{{asset('/img/business.png')}}" alt="">
+            <p class="header__links__item__text">Mijn business</p>
+        </div>
+
+        <div class="header__links__item">
+            <img class="header__links__item__img" src="{{asset('/img/roadmap.png')}}" alt="">
+            <p class="header__links__item__text">Roadmap</p>
+        </div>
+
+        <div class="header__links__item">
+            <img class="header__links__item__img" src="{{asset('/img/comm.png')}}" alt="">
+            <p class="header__links__item__text">Community</p>
+        </div>
+
+        <div class="header__links__item">
+            <img class="header__links__item__img" src="{{asset('/img/chat.png')}}" alt="">
+            <p class="header__links__item__text">Chat</p>
+        </div>
+
+        <div class="header__links__item">
+            <img class="header__links__item__img" src="{{asset('/img/contacten.png')}}" alt="">
+            <p class="header__links__item__text">Contacten</p>
+        </div>
+
+        <div class="header__links__item">
+            <img class="header__links__item__img" src="{{asset('/img/instellingen.png')}}" alt="">
+            <p class="header__links__item__text">Instellingen</p>
+        </div>
+
+    </div>
+</div>

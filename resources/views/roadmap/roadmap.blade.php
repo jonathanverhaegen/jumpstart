1cb3d876c260d9c8233bbb5ab53f90064eb469f2
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2022-05-22 12:12:19 +0200

    test

--- resources/views/roadmap/roadmap.blade.php
+++ resources/views/roadmap/roadmap.blade.php
@@ -89,7 +89,16 @@
                 <a @if($roadmap->stage > 6) href="" data-stage="7" @else style="opacity:0.4" @endif  class="roadmap__stage roadmap__stage--7">
                     <div class="roadmap__stage__title roadmap__stage__title--left">Ondernemingsnummer</div>
                     <div class="roadmap__stage__number">7</div>
+                    <img src="{{asset('img/stengel.png')}}" alt="stengel" class="stengel__icon7">
                 </a>
+
+
+                <a @if($roadmap->stage > 7) href="" data-stage="8" @else style="opacity:0.4" @endif  class="roadmap__stage roadmap__stage--8">
+                    <div class="roadmap__stage__number">8</div>
+                    <div class="roadmap__stage__title roadmap__stage__title--right">Student-zelfstandige</div>
+                    <img src="{{asset('img/stengel.png')}}" alt="stengel" class="stengel__icon8">
+                </a>
+
 
                 <a class="roadmap__next" href="">
                     <img class="nextBtn" src="{{asset('/img/knop3.png')}}" alt="">

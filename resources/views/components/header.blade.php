135254930ecfb5b20993104bd29da0f88d97a8a4
surinde <r0657978@student.thomasmore.be>
2022-05-22 21:33:38 +0200

    build nav mobile

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,41 @@
 <div class="header--mob">
     <div class="header--mob__img">
+    <div class="header__links__mob">
+        <a href="/dashboard" class="header__links__item__mob">
+            <img class="header__links__item__img__mob" src="{{asset('/img/business.png')}}" alt="">
+            <p class="header__links__item__text__mob">Mijn business</p>
+        </a>
+
+        <a href="/roadmap" class="header__links__item__mob">
+            <img class="header__links__item__img__mob" src="{{asset('/img/roadmap.png')}}" alt="">
+            <p class="header__links__item__text__mob">Roadmap</p>
+        </a>
+
+        <a href="/community" class="header__links__item__mob">
+            <img class="header__links__item__img__mob" src="{{asset('/img/comm.png')}}" alt="">
+            <p class="header__links__item__text__mob">Community</p>
+        </a>
+
+        <a href="/chat" class="header__links__item__mob">
+            <img class="header__links__item__img__mob" src="{{asset('/img/chat.png')}}" alt="">
+            <p class="header__links__item__text__mob">Chat</p>
+        </a>
+
+        <a href="/contacten" class="header__links__item__mob">
+            <img class="header__links__item__img__mob" src="{{asset('/img/contacten.png')}}" alt="">
+            <p class="header__links__item__text__mob">Contacten</p>
+        </a>
+
+        <a href="/instellingen" class="header__links__item__mob">
+            <img class="header__links__item__img__mob" src="{{asset('/img/instellingen.png')}}" alt="">
+            <p class="header__links__item__text__mob">Instellingen</p>
+        </a>
+
+        <div class="header__logout__mob">
+            <a class="btn btn--logout" href="/logout">Afmelden</a>
+        </div>
+
+    </div>
 
     </div>
 </div>

714d942e780bc965b10000b77db315ef392bd29c
Bob Storms <bob.storms@hotmail.be>
2022-06-02 18:08:28 +0200

    styling: detail page of settings page

--- resources/views/instellingen/instellingen.blade.php
+++ resources/views/instellingen/instellingen.blade.php
@@ -83,7 +83,36 @@
             </div>
 
             <div class="settings__detail">
-                Dit zijn de instellingen.
+                <div class="settings__detail__profile-picture">
+                    <img src="./img/Laurens.png" alt="Profielfoto">
+                    <div class="settings__detail__profile-picture__name">
+                        <span class="settings__user-name">Kelly Smith</span>
+                        <label class="settings__upload-profile-picture"><input class="avatar" name="avatar" type="file" accept=".png, .jpg, .jpeg"/>Profielfoto wijzigen</label>
+                    </div>
+                </div>
+
+                <div class="settings__detail__info">
+                    <form action="#" method="POST">
+                        
+                        <label for="name">Naam</label>
+                        <input type="text" name="name" id="name" class="settings__detail__info__input" value="Kelly Smith">
+
+                        <label for="company-name">Bedrijfsnaam</label>
+                        <input type="text" name="company-name" id="company-name" class="settings__detail__info__input" value="Kelly Smith Design">
+
+                        <label for="email">E-mailadres</label>
+                        <input type="email" name="email" id="email" class="settings__detail__info__input" value="kelly.smith@gmail.com">
+
+                        <label for="phone">Telefoonnummer</label>
+                        <input type="text" name="phone" id="phone" class="settings__detail__info__input" value="0488 17 57 41">
+
+                        <label for="bio">Bio</label>
+                        <textarea name="bio" id="bio" cols="30" rows="8">Kelly Smith Design is een jong bedrijf dat draait rond diversiteit, dit is terug te vinden in de designs. De focus van KSD ligt op branding en rebranding van merken en producten. Neem gerust een kijkje op Behance voor voorbeelden.</textarea>
+
+                        <input type="submit" value="Verzenden">
+
+                    </form>
+                </div>
             </div>
 
         </div>

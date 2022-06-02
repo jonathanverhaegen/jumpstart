f38929faa54ba4aae3a64f6227583b04f48d416e
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2022-06-02 12:04:45 +0200

    attachment reaction saved in storage

--- app/Http/Livewire/Reactions.php
+++ app/Http/Livewire/Reactions.php
@@ -28,7 +28,7 @@ class Reactions extends Component
             if(!empty($this->attachment)){
                 $file = $this->attachment;
                 $imageSrc = time().'.'.$file->extension();
-                $this->attachment->storeAs('/public/attachments', $imageSrc);
+                $this->attachment->storeAs('attachments', $imageSrc);
 
                 //attachment opslaan in database
                 $newAttach = new AttachmentsReaction();

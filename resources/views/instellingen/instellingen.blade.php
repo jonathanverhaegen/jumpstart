9469c12d3e83b096173acb86e9fa645b023d7910
Bob Storms <bob.storms@hotmail.be>
2022-06-02 16:23:47 +0200

    styling: left side of settinsg for desktop

--- resources/views/instellingen/instellingen.blade.php
+++ resources/views/instellingen/instellingen.blade.php
@@ -53,15 +53,39 @@
         @endcomponent
         @endif
 
+        <div class="settings__container">
 
+            <div class="settings__list">
+                <a href="#" class="settings__list__item settings__list__item--active">
+                    Profiel bewerken
+                </a>
+                <a href="/instellingen/wachtwoord-wijzigen" class="settings__list__item">
+                    Wachtwoord wijzigen
+                </a>
+                <a href="#" class="settings__list__item">
+                    Links en websites
+                </a>
+                <a href="#" class="settings__list__item">
+                    Notificaties
+                </a>
+                <a href="#" class="settings__list__item">
+                    Privacy en beveiliging
+                </a>
+                <a href="/instellingen/statuut" class="settings__list__item">
+                    Statuut
+                </a>
+                <a href="#" class="settings__list__item">
+                    Contact
+                </a>
+                <a href="#" class="settings__list__item">
+                    Help
+                </a>
+            </div>
 
+            <div class="settings__detail">
+                Dit zijn de instellingen.
+            </div>
 
-
-      <div>
-          <h1>Hier staan de instellingen</h1>
-      </div>
-
-
-
+        </div>
 
 @endsection

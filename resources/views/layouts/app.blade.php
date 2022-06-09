a26af2ffa7bd96186e21a49c627ff12f04f73634
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2022-06-09 15:22:38 +0200

    added favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+    <link rel="icon" type="image/x-icon" href="/img/favicon.png">
     <link rel="stylesheet" href="css/tailwind/tailwind.min.css">
     <link rel="stylesheet" href="{{ URL::asset('css/tailwind/tailwind.min.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('css/reset.css') }}">

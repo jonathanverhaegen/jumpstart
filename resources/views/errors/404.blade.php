d4b3d19015db65133ad41203fb29dbe05c23a170
Bob Storms <bob.storms@hotmail.be>
2022-06-12 16:52:38 +0200

    add 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,1 +1,21 @@
-<h1>test</h1>
+@extends('layouts/app')
+
+@section('title', 'Pagina niet gevonden | Jumpstart')
+
+<style>
+    html, body {
+        height: 100%;
+        width: 100%;
+    }
+</style>
+
+@section('content')
+
+<div class="error-page">
+    <img src="img/illustration-error.png" alt="Niet gevonden" class="error-page__illustration">
+    <h1>404</h1>
+    <h2>Pagina niet gevonden</h2>
+    <a href="/dashboard" class="error-page__button">Ga terug naar Jumpstart</a>
+</div>
+
+@endsection

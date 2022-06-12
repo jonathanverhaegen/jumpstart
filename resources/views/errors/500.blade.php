4bfa0b3222e48c6c57938159a942124eeb262944
Bob Storms <bob.storms@hotmail.be>
2022-06-12 17:12:59 +0200

    add 500 error page

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts/app')
+
+@section('title', 'Server error | Jumpstart')
+
+<style>
+    html, body {
+        height: 100%;
+        width: 100%;
+    }
+</style>
+
+@section('content')
+
+<div class="error-page">
+    <img src="img/illustration-error.png" alt="Niet gevonden" class="error-page__illustration">
+    <h1>500</h1>
+    <h2>Server error</h2>
+    <a href="/dashboard" class="error-page__button">Ga terug naar Jumpstart</a>
+</div>
+
+@endsection
